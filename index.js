daf0b393b6ac1e48fd4876bf89db51890d24453f
Nikhil Jose <nikhiljose12345@gmail.com>
2022-08-31 23:05:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+async function getData(){
+    let ifsc=document.getElementById("ifsc-input").value
+    fetch(`https://ifsc.razorpay.com/${ifsc}`)
+        .then((res) => {
+            if(res.status==404){
+                throw("error");
+            }
+            else{
+                return res.json()
+            }
+        })
+        .then(data=>{
+            console.log(data)
+            document.getElementById("ifsc").innerText = data.IFSC;
+            document.getElementById("bank").innerText=data.BANK
+            document.getElementById("branch").innerText=data.BRANCH
+            document.getElementById("city").innerText=data.CITY
+            document.getElementById("district").innerText=data.DISTRICT
+            document.getElementById("address").innerText=data.ADDRESS
+            document.getElementById("micr").innerText=data.MICR
+        })
+        .catch(err=>{
+            document.getElementById("ifsc").innerText = "Please Enter a Valid IFSC Code";
+            document.getElementById("bank").innerText = "...";
+            document.getElementById("branch").innerText = "...";
+            document.getElementById("city").innerText = "...";
+            document.getElementById("district").innerText = "...";
+            document.getElementById("address").innerText = "...";
+            document.getElementById("micr").innerText = "...";
+        })
+}
